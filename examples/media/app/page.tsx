34e25163707c093d84d8947e9a924670bfac01f0
Jimmy Madrigal <jimmymadrigal+github@gmail.com>
2024-03-16 10:20:22 -0600

    feat: example media
    Signed-off-by: Jimmy Madrigal <jimmymadrigal+github@gmail.com>

---
+++ examples/media/app/page.tsx
@@ -0,0 +1,58 @@
+'use server';
+
+import { headers } from 'next/headers';
+import {
+  FrameContainer,
+  FrameImage,
+  FrameButton,
+  useFramesReducer,
+  getPreviousFrame,
+  FrameInput,
+  type NextServerPageProps,
+} from 'frames.js/next/server';
+
+export default async function Page(props: NextServerPageProps) {
+  const headersList = headers();
+  const previousFrame = getPreviousFrame(props.searchParams);
+  const [state] = useFramesReducer((state) => state, {}, previousFrame);
+
+  const origin =
+    typeof window !== 'undefined'
+      ? window.location.origin
+      : `${headersList.get('x-forwarded-proto')}://${headersList.get('x-forwarded-host')}` ??
+        '';
+
+  let src = `${origin}/png-sample.png`;
+  switch (previousFrame.postBody?.untrustedData.buttonIndex) {
+    case 1:
+      src = `${origin}/gif-sample.gif`;
+      break;
+    case 2:
+      src = `${origin}/svg-sample.svg`;
+      break;
+    case 3:
+      src = `${origin}/svg-malicious-sample.svg`;
+      break;
+    case 4:
+      if (previousFrame.postBody?.untrustedData.inputText?.trim()) {
+        src = previousFrame.postBody?.untrustedData.inputText?.trim() ?? '';
+      }
+      break;
+  }
+
+  return (
+    <FrameContainer
+      postUrl="/frames"
+      pathname="/"
+      state={state}
+      previousFrame={previousFrame}
+    >
+      <FrameImage src={src} />
+      <FrameInput text="Paste your media url" />
+      <FrameButton>GIF</FrameButton>
+      <FrameButton>SVG</FrameButton>
+      <FrameButton>SVG (Dangerous)</FrameButton>
+      <FrameButton>Custom</FrameButton>
+    </FrameContainer>
+  );
+}
